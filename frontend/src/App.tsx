8481e0bc65998fc6aac5bde5941bff85c39e2f2d
CesarOrtiz1994 <cesarortize94@gmail.com>
2025-10-01 13:59:37 -0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">✅ Tailwind v4 funcionando</h1>
+    </div>
+  )
+}
+
+export default App
